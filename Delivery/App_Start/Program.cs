5653e86a1ea2e95ee08f2ee65d8e0eb3214c4104
Leo <leonardo.xavier@dextra-sw.com>
2015-06-06 11:56:36 -0300

    Owin/Katana

---
+++ Delivery/App_Start/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin.Hosting;
+
+namespace Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpLocalHost = "http://localhost:9090";
+            using (WebApp.Start<Startup>(httpLocalHost))
+            {
+                System.Console.WriteLine("Listening on " + httpLocalHost);
+                System.Console.WriteLine("Press [enter] to quit...");
+                System.Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Delivery/App_Start/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin.Cors;
+using Owin;
+
+namespace Delivery
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+        }
+    }
+}
