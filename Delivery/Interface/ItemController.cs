fae893d6c0d4f525cf213b18e73d07969f6c034d
Leo <leonardo.xavier@dextra-sw.com>
2015-06-07 13:09:50 -0300

    Simple Get

---
+++ Delivery/Interface/ItemController.cs
@@ -0,0 +1,18 @@
+﻿using Delivery.Infrastructure;
+using Delivery.Infrastructure.Entities;
+using Nancy;
+
+namespace Delivery.Interface
+{
+    public class ItemController : NancyModule
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public ItemController(UnitOfWork unitOfWork) : base("/item")
+        {
+            _unitOfWork = unitOfWork;
+
+            Get["/"] = x => _unitOfWork.Repository<Item>().All();
+        }
+    }
+}
